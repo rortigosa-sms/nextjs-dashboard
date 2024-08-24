d14b94a879ec558f13b3646929d47193f0c04889
Rodrigo Ortigosa <rortigosa@sms-sudamerica.com>
2024-08-24 18:22:52 -0300

    feat: first commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { montserrat } from "./ui/fonts";
+import "@/app/ui/global.css";
+
+export const metadata: Metadata = {
+  title: "Curso Nextjs",
+  description: "Curso de midudev de Nextjs",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} antialiased`}>
+        {children}
+        <footer className="py-10 flex justify-center items-center">
+          Hecho con ❤️ por la gente de Vercel
+        </footer>
+      </body>
+    </html>
+  );
+}
